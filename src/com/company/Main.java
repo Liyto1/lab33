0405d1af24e356612d770abf77d0de31bb96f357
Liyto1 <lev.liv2@gmai.com>
2018-10-17 14:19:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+import java.util.Scanner;
+
+
+//////////////////////////////////lab3zad1
+
+/*public class Main {
+    public static <string> void main(String[] args) {
+
+        double c=0, d=0;
+        for( int n=2 ; n<=5 ; n++){
+        for ( c = 1 ; c<=100 ; c++){
+        for (int a = 1 ; a <=10 ; a++){
+        for (int b = 1 ; b <= 10 ; b++){
+            d=(Math.pow(a,n)+Math.pow(b,n));
+             if (Math.pow(c,n) == d) {
+             System.out.println(a + "^" + n + "+ " + b + "^" + n + "=" + d); }
+                    }}}}}*/
+
+
+//////////////////////////////////lab3zad2
+
+/*public class Main {
+    public static void main(String[] args) {
+        float b=0;
+        int a=2;
+        for(float i=1f ; i<=10000f ; i++) {
+            if(a % 2==0) {
+                b+= 1 / i;
+            }
+            else{
+                b-= 1 / i;
+            }
+        }
+        System.out.print("Результат:"+b);
+    }}*/
